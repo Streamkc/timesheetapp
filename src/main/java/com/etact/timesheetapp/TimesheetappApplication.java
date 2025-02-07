830d810dd6cdc6fcd25b9bd2ea45f955ec91096c
Kecheng He <river0612@gmail.com>
2025-02-07 12:37:19 +0800

    init

---
+++ src/main/java/com/etact/timesheetapp/TimesheetappApplication.java
@@ -0,0 +1,13 @@
+package com.etact.timesheetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etact/timesheetapp/TimesheetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etact.timesheetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
